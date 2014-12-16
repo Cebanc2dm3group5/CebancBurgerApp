b2e9a7d10f0c7ed68fa35e565da433362ebc952e
uesteibar <uesteibar@gmail.com>
2014-12-16 10:14:29 +0100

    work on last view

--- src/com/grupo5/cebancburger/OrderRevisionActivity.java
+++ src/com/grupo5/cebancburger/OrderRevisionActivity.java
@@ -2,33 +2,35 @@ package com.grupo5.cebancburger;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.grupo5.cebancburger.adapters.CardArrayAdapter;
+import com.grupo5.cebancburger.viewmodels.Card;
 
 public class OrderRevisionActivity extends Activity {
 
+	private static final String TAG = "CardListActivity";
+	private CardArrayAdapter cardArrayAdapter;
+	private ListView listView;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.order_final_layout);
-	}
+		listView = (ListView) findViewById(R.id.card_listView);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.order_revision, menu);
-		return true;
-	}
+		listView.addHeaderView(new View(this));
+		listView.addFooterView(new View(this));
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		cardArrayAdapter = new CardArrayAdapter(getApplicationContext(),
+				R.layout.list_item_card);
+
+		for (int i = 0; i < 10; i++) {
+			Card card = new Card("Card " + (i + 1) + " Line 1", "Card "
+					+ (i + 1) + " Line 2", i*10);
+			cardArrayAdapter.add(card);
 		}
-		return super.onOptionsItemSelected(item);
+		listView.setAdapter(cardArrayAdapter);
 	}
 }
---
+++ src/com/grupo5/cebancburger/viewmodels/Card.java
@@ -0,0 +1,26 @@
+package com.grupo5.cebancburger.viewmodels;
+
+public class Card {
+	private String line1;
+	private String line2;
+	private String price;
+
+	public Card(String line1, String line2, int price) {
+		this.line1 = line1;
+		this.line2 = line2;
+		this.price = Integer.toString(price);
+	}
+
+	public String getLine1() {
+		return line1;
+	}
+
+	public String getLine2() {
+		return line2;
+	}
+
+	public String getPrice() {
+		return price + " €";
+	}
+
+}
