88c0e1c27f089591cc995c7861997c4732880d44
uesteibar <uesteibar@gmail.com>
2014-12-19 11:06:20 +0100

    create activities and declare in manifest

---
+++ src/com/grupo5/cebancburger/BebidaSelectActivity.java
@@ -0,0 +1,14 @@
+package com.grupo5.cebancburger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BebidaSelectActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bebida_select_layout);
+
+	}
+}
