b8508425be3c2685c3c2ba435be19fc9a1cd163b
Gorriti <gorriti@hotmail.com>
2015-01-08 13:32:21 +0100

    create OK Button

--- src/com/grupo5/cebancburger/MainActivity.java
+++ src/com/grupo5/cebancburger/MainActivity.java
@@ -1,6 +1,8 @@
 package com.grupo5.cebancburger;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,11 +17,14 @@ public class MainActivity extends Activity {
 
 	Button btnNext, btnExit;
 	EditText edtName, edtAddress, edtPhone;
+	AlertDialog.Builder alert;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		alert = new AlertDialog.Builder(this);
 		
 		edtName = (EditText) findViewById(R.id.edtName);
 		edtAddress = (EditText) findViewById(R.id.edtAddress);
@@ -44,16 +49,28 @@ public class MainActivity extends Activity {
 				String name = edtName.getText().toString();
 				String address = edtAddress.getText().toString();
 				String phone = edtPhone.getText().toString();
-				Cliente cliente = new Cliente(name, address, phone);
-				pedido.setCliente(cliente);
-				// create intent
-				Intent intent = new Intent(getApplicationContext(),
-						BurgerSelectActivity.class);
-				// add data to intent
-				intent.putExtra("pedido", pedido);
 				
-				// start activity
-				startActivityForResult(intent, 1);
+				if (name.equals("") || address.equals("") || phone.equals("")){
+					
+		            alert.setTitle("¡CUIDADO!")
+		            	.setMessage("Introduce todos tus datos")
+		            	.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		                    public void onClick(DialogInterface dialog, int id) {
+		                        //do things
+		                    }})
+		            	.show();
+				}else{
+					Cliente cliente = new Cliente(name, address, phone);
+					pedido.setCliente(cliente);
+					// create intent
+					Intent intent = new Intent(getApplicationContext(),
+							BurgerSelectActivity.class);
+					// add data to intent
+					intent.putExtra("pedido", pedido);
+					
+					// start activity
+					startActivityForResult(intent, 1);
+				}
 			}
 		});
 	}
