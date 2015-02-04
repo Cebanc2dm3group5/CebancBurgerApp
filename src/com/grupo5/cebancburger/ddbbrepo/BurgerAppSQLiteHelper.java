655d23f4d3eb19288cd4719400243ba168f5d678
Gorriti <gorriti@hotmail.com>
2015-02-04 11:22:28 +0100

    create BurgerTypeTable

--- src/com/grupo5/cebancburger/ddbbrepo/BurgerAppSQLiteHelper.java
+++ src/com/grupo5/cebancburger/ddbbrepo/BurgerAppSQLiteHelper.java
@@ -5,13 +5,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.grupo5.cebancburger.ddbbrepo.tables.CustomerTable;
-import com.grupo5.cebancburger.ddbbrepo.tables.UserTable;
+import com.grupo5.cebancburger.ddbbrepo.tables.*;
 
 public class BurgerAppSQLiteHelper extends SQLiteOpenHelper {
 
 	UserTable userTable = new UserTable();
 	CustomerTable customerTable = new CustomerTable();
+	BurgerTypeTable burgerTypeTable = new BurgerTypeTable();
 
 	public BurgerAppSQLiteHelper(Context context, String name,
 			CursorFactory factory, int version) {
@@ -22,6 +22,7 @@ public class BurgerAppSQLiteHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(userTable.getSqlCreate());
 		db.execSQL(customerTable.getSqlCreate());
+		db.execSQL(burgerTypeTable.getSqlCreate());
 	}
 
 	@Override
@@ -29,10 +30,12 @@ public class BurgerAppSQLiteHelper extends SQLiteOpenHelper {
 		// Se elimina la versión anterior de la tabla
 		db.execSQL(userTable.getSqlUpdate());
 		db.execSQL(customerTable.getSqlUpdate());
+		db.execSQL(burgerTypeTable.getSqlUpdate());
 
 		// Se crea la nueva versión de la tabla
 		db.execSQL(userTable.getSqlCreate());
 		db.execSQL(customerTable.getSqlCreate());
+		db.execSQL(burgerTypeTable.getSqlCreate());
 	}
 
 }
